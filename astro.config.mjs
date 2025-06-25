b91d29a9d304e0c91da38bd8d47e15f88ad939ba
ishira-web <ishira.pahasara@icloud.com>
2025-06-25 20:50:00 +0530

    chore: update dependencies to use Vercel adapter and remove Netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,22 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
-import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
-
+import vercel from "@astrojs/vercel/serverless";
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
 
 import svelte from "@astrojs/svelte";
 
 export default defineConfig({
-
+  output: "server",
+   adapter: vercel(),
   integrations: [
     sitemap(),
     solidJs(),
     UnoCSS({ injectReset: true }),
     icon(),
     svelte(),
-    netlify(),
   ],
   markdown: {
     remarkPlugins: [remarkReadingTime],
